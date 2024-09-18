34e8f3448d5e12ee13e091acc914fa20c7af151a
JebetIvy <138754525+IvyJebet@users.noreply.github.com>
2024-09-18 15:39:57 +0300

    StudentsController.js  File

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -7,7 +7,7 @@ const VALID_MAJORS = ['CS', 'SWE'];
 
 /**
  * Contains the student-related route handlers.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Ivy Jebet <https://github.com/IvyJebet>
  */
 class StudentsController {
   static getAllStudents(request, response) {
